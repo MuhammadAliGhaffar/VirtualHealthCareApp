a00ad7d87b977c49744cd75f47ae3226cd58960f
MuhammadAliGhaffar <alibrohi999@gmail.com>
2022-04-25 01:41:48 +0500

    user location

--- app/src/main/java/com/example/vhaapp/ui/NearestHospitalsFragment.kt
+++ app/src/main/java/com/example/vhaapp/ui/NearestHospitalsFragment.kt
@@ -1,17 +1,50 @@
 package com.example.vhaapp.ui
 
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.vhaapp.R
-import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.geojson.Point
+import com.mapbox.maps.*
+import com.mapbox.maps.plugin.animation.MapAnimationOptions
+import com.mapbox.maps.plugin.animation.camera
+import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.navigation.base.options.NavigationOptions
+import com.mapbox.navigation.core.MapboxNavigation
+import com.mapbox.navigation.core.trip.session.LocationMatcherResult
+import com.mapbox.navigation.core.trip.session.LocationObserver
+import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
 
-class NearestHospitalsFragment : Fragment() {
+class NearestHospitalsFragment : Fragment(),PermissionsListener {
+
+    lateinit var permissionsManager: PermissionsManager
+
+    private val navigationLocationProvider = NavigationLocationProvider()
 
     var mapView: MapView? = null
+    private lateinit var mapboxNavigation: MapboxNavigation
+
+
+    private val locationObserver = object : LocationObserver {
+        override fun onNewRawLocation(rawLocation: Location) {
+        }
+
+        override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
+            val enhancedLocation = locationMatcherResult.enhancedLocation
+            navigationLocationProvider.changePosition(
+                enhancedLocation,
+                locationMatcherResult.keyPoints,
+            )
+            updateCamera(enhancedLocation)
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +58,66 @@ class NearestHospitalsFragment : Fragment() {
 
     private fun initView(view: View) {
         mapView = view.findViewById(R.id.mapView)
+
+        if (PermissionsManager.areLocationPermissionsGranted(context)) {
+            mapView?.location?.apply {
+                setLocationProvider(navigationLocationProvider)
+                enabled = true
+            }
+            initNavigation()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(requireActivity())
+        }
+        initStyle()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initNavigation() {
+        mapboxNavigation = MapboxNavigation(
+            NavigationOptions.Builder(requireContext())
+                .accessToken(getString(R.string.mapbox_access_token))
+                .build()
+        ).apply {
+            startTripSession()
+            registerLocationObserver(locationObserver)
+        }
+    }
+
+    private fun initStyle() {
         mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
 
+    }
+
+    private fun updateCamera(location: Location) {
+        val mapAnimationOptions = MapAnimationOptions.Builder().duration(1500L).build()
+        mapView?.camera?.easeTo(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(location.longitude, location.latitude))
+                .zoom(12.0)
+                .padding(EdgeInsets(500.0, 0.0, 0.0, 0.0))
+                .build(),
+            mapAnimationOptions
+        )
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapboxNavigation.stopTripSession()
+        mapboxNavigation.unregisterLocationObserver(locationObserver)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
     }
 }
