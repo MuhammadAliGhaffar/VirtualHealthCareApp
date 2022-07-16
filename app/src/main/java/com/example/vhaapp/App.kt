2b071eea45d9e4a5b0e0e03000c8068d7b30a082
MuhammadAliGhaffar <alibrohi999@gmail.com>
2022-07-16 13:22:56 +0500

    Removed mapbox + added webview

--- app/src/main/java/com/example/vhaapp/App.kt
+++ app/src/main/java/com/example/vhaapp/App.kt
@@ -2,8 +2,6 @@ package com.example.vhaapp
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
-import com.mapbox.android.core.location.LocationEngineProvider
-import com.mapbox.search.MapboxSearchSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -13,10 +11,5 @@ class App :Application() {
         super.onCreate()
         //forcefully set light theme in app
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        MapboxSearchSdk.initialize(
-            application = this,
-            accessToken = getString(R.string.mapbox_access_token),
-            locationEngine = LocationEngineProvider.getBestLocationEngine(this)
-        )
     }
 }
--- app/src/main/java/com/example/vhaapp/ui/NearestHospitalsFragment.kt
+++ app/src/main/java/com/example/vhaapp/ui/NearestHospitalsFragment.kt
@@ -1,75 +1,20 @@
 package com.example.vhaapp.ui
 
-import android.annotation.SuppressLint
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.vhaapp.R
-import com.mapbox.android.core.permissions.PermissionsListener
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.geojson.Point
-import com.mapbox.maps.CameraOptions
-import com.mapbox.maps.EdgeInsets
-import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
-import com.mapbox.maps.plugin.animation.MapAnimationOptions
-import com.mapbox.maps.plugin.animation.camera
-import com.mapbox.maps.plugin.locationcomponent.location
-import com.mapbox.navigation.base.options.NavigationOptions
-import com.mapbox.navigation.core.MapboxNavigation
-import com.mapbox.navigation.core.trip.session.LocationMatcherResult
-import com.mapbox.navigation.core.trip.session.LocationObserver
-import com.mapbox.navigation.ui.maps.location.NavigationLocationProvider
-import com.mapbox.search.*
-import com.mapbox.search.result.SearchResult
 
-class NearestHospitalsFragment : Fragment(),PermissionsListener {
+class NearestHospitalsFragment : Fragment() {
 
-    lateinit var permissionsManager: PermissionsManager
-
-    private val navigationLocationProvider = NavigationLocationProvider()
-
-    var mapView: MapView? = null
-    private lateinit var mapboxNavigation: MapboxNavigation
-
-    private lateinit var categorySearchEngine: CategorySearchEngine
-    private lateinit var searchRequestTask: SearchRequestTask
-
-
-
-    private val locationObserver = object : LocationObserver {
-        override fun onNewRawLocation(rawLocation: Location) {
-        }
-
-        override fun onNewLocationMatcherResult(locationMatcherResult: LocationMatcherResult) {
-            val enhancedLocation = locationMatcherResult.enhancedLocation
-            navigationLocationProvider.changePosition(
-                enhancedLocation,
-                locationMatcherResult.keyPoints,
-            )
-            updateCamera(enhancedLocation)
-        }
-    }
-
-    private val searchCallback: SearchCallback = object : SearchCallback {
-
-        override fun onResults(results: List<SearchResult>, responseInfo: ResponseInfo) {
-            if (results.isEmpty()) {
-                Log.i("SearchApiExample", "No category search results")
-            } else {
-                Log.i("SearchApiExample", "Category search results: $results")
-            }
-        }
-
-        override fun onError(e: Exception) {
-            Log.i("SearchApiExample", "Search error", e)
-        }
-    }
-
+    private lateinit var webViewMap:WebView
+    private lateinit var imgBack: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -82,77 +27,20 @@ class NearestHospitalsFragment : Fragment(),PermissionsListener {
     }
 
     private fun initView(view: View) {
-        mapView = view.findViewById(R.id.mapView)
+        webViewMap = view.findViewById(R.id.webViewMap)
+        imgBack = view.findViewById(R.id.imgBack)
 
-        if (PermissionsManager.areLocationPermissionsGranted(context)) {
-            categorySearchEngine = MapboxSearchSdk.getCategorySearchEngine()
+        webViewMap.webViewClient = WebViewClient()
+        webViewMap.loadUrl("https://serene-mount-rainier-21655.herokuapp.com/")
+        webViewMap.settings.javaScriptEnabled = true
+        webViewMap.settings.setSupportZoom(true)
 
-            searchRequestTask = categorySearchEngine.search(
-                "hospital",
-                CategorySearchOptions(limit = 20),
-                searchCallback
-            )
-
-            mapView?.location?.apply {
-                setLocationProvider(navigationLocationProvider)
-                enabled = true
-            }
-            initNavigation()
-
-        } else {
-            permissionsManager = PermissionsManager(this)
-            permissionsManager.requestLocationPermissions(requireActivity())
-        }
-        initStyle()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun initNavigation() {
-        mapboxNavigation = MapboxNavigation(
-            NavigationOptions.Builder(requireContext())
-                .accessToken(getString(R.string.mapbox_access_token))
-                .build()
-        ).apply {
-            startTripSession()
-            registerLocationObserver(locationObserver)
+        imgBack.setOnClickListener {
+            if (webViewMap.canGoBack())
+                webViewMap.goBack()
+            else
+                findNavController().navigateUp()
         }
     }
 
-    private fun initStyle() {
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
-
-    }
-
-    private fun updateCamera(location: Location) {
-        val mapAnimationOptions = MapAnimationOptions.Builder().duration(1500L).build()
-        mapView?.camera?.easeTo(
-            CameraOptions.Builder()
-                .center(Point.fromLngLat(location.longitude, location.latitude))
-                .zoom(12.0)
-                .padding(EdgeInsets(500.0, 0.0, 0.0, 0.0))
-                .build(),
-            mapAnimationOptions
-        )
-    }
-
-
-    override fun onDestroyView() {
-        searchRequestTask.cancel()
-        super.onDestroyView()
-        mapboxNavigation.stopTripSession()
-        mapboxNavigation.unregisterLocationObserver(locationObserver)
-    }
-
-    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
-    }
-
-    override fun onPermissionResult(granted: Boolean) {
-    }
 }
